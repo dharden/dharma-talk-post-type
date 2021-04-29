4d0a3264a4b58a573eb14b0173993663880044c6
Dan Harden <daniel.harden@gmail.com>
2021-04-29 09:39:30 -0700

    initial commit

---
+++ content-dharma-talk.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * The template for displaying posts in the Dharma Talk post format
+ *
+ * @package BZC
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+
+<?php // set up some custom fields and other useful strings
+$author = get_the_author();
+$date = esc_attr( get_the_date( 'c' ) );
+$pretty_date = esc_html( get_the_date() );
+$youtube_link = esc_attr( get_post_meta( get_the_ID(), 'dt_youtube_link', true ) );
+$episode_content = get_the_powerpress_content();
+$format_class = [];
+if ( $youtube_link ) { array_push($format_class, 'format-video'); };
+if ( $episode_content ) { array_push($format_class, 'format-audio'); };
+$bio = nl2br( get_the_author_meta( 'description' ) );
+$the_content = get_the_content(
+				sprintf(
+					/* translators: %s: Post title. */
+					__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ),
+					the_title( '<span class="screen-reader-text">', '</span>', false )
+				)
+			);
+
+			wp_link_pages(
+				array(
+					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+				)
+			);
+$default_description = "<p>A talk given at Berkeley Zen Center on " . $pretty_date . " by " . $author . ".</p>";
+$talk_description = $the_content ? $the_content : $default_description;
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class($format_class); ?>>
+	<header class="entry-header">
+		<div class="entry-meta">
+			<a class="cat-links" href="<?php echo esc_url( get_post_type_archive_link( 'dharma-talk' ) ); ?>">Dharma Talks</a>
+		</div><!-- .entry-meta -->
+			<?php
+				if ( is_single() ) :
+					the_title( '<h1 class="entry-title">', '</h1>' );
+					else :
+						the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+					endif;
+					?>
+
+				<div class="entry-meta">
+					<?php
+				if ( is_sticky() && is_home() && ! is_paged() ) {
+					echo '<span class="featured-post">' . __( 'Sticky', 'twentyfourteen' ) . '</span>';
+				}
+
+				// Set up and print post meta information.
+				?>
+				<?php
+				printf(
+					'<span class="byline"><span class="author vcard"><a class="url fn n" href="%1$s" rel="author">%2$s</a></span></span><span class="entry-date"><a href="%3$s" rel="bookmark"><time class="entry-date" datetime="%4$s">%5$s</time></a></span>',
+					esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+					$author,
+					esc_url( get_permalink() ),
+					$date,
+					$pretty_date
+				);
+			?>
+			<?php if ( $youtube_link ) {?>
+			<span class="post-format video">
+				<a class="entry-format" href="<?php echo esc_url( get_post_format_link( 'video' ) ); ?>"><?php echo get_post_format_string( 'video' ); ?></a>
+			</span>
+			<?php }; ?>
+			<?php if ( $episode_content ) {?>
+			<span class="post-format audio">
+				<a class="entry-format" href="<?php echo esc_url( get_post_format_link( 'audio' ) ); ?>"><?php echo get_post_format_string( 'audio' ); ?></a>
+			</span>
+			<?php }; ?>
+			<?php edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' ); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+	<div class="entry-content">
+		<?php if ( $youtube_link && !is_archive() ) { ?>
+		<?php echo do_shortcode("[youtube " . $youtube_link . "]" ); ?>
+		<?php }; ?>
+		<?php if ( $talk_description ) echo $talk_description; ?>
+		<?php if( $episode_content && !is_archive()) { ?>
+		<div class="podcast">
+			<h2>Audio</h2>
+			<p>Listen to an audio-only version of this talk here: </p>
+			<?php echo $episode_content ?>
+			<p>Subscribe to The Berkeley Zen Center Podcast: <a href="https://berkeleyzencenter.org/feed/podcast/" title="Subscribe via RSS" rel="nofollow">RSS</a></p>
+		</div>
+		<?php } ?>
+	</div><!-- .entry-content -->
+	<?php if( $bio && !is_archive() ) { ?>
+	<div class="about">
+		<?php if ( has_wp_user_avatar(get_the_author_meta('ID') ) ){ ?>
+		<img class="avatar circle" src="<?php echo get_wp_user_avatar_src(get_the_author_meta('ID'), 128); ?>" width="128" height="128" alt="<?php echo $author; ?>" />
+		<?php } ?>
+		<div class="bio">
+			<h3>About <?php echo $author; ?></h3>
+			<p><?php echo $bio; ?></p>
+			<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) ?>">More from <?php echo $author; ?></a>
+		</div>
+	</div><!-- .entry-meta -->
+	<?php } ?>
+
+</article><!-- #post-<?php the_ID(); ?> -->
---
+++ dharma-talk-post-type.php
@@ -0,0 +1,126 @@
+<?php
+/*
+Plugin Name: Dharma Talk Post Type
+Description: Add post types for dharma talks
+Author: Dan Harden
+*/
+// Hook <strong>custom_post_dharma_talk()</strong> to the init action hook
+add_action( 'init', 'bzc_custom_post_dharma_talk' );
+// The custom function to register a dharma talk post type
+function bzc_custom_post_dharma_talk() {
+// Set the labels, this variable is used in the $args array
+$labels = array(
+'name'               => __( 'Dharma Talks' ),
+'singular_name'      => __( 'Dharma Talk' ),
+'add_new'            => __( 'Add New Dharma Talk' ),
+'add_new_item'       => __( 'Add New Dharma Talk' ),
+'edit_item'          => __( 'Edit Dharma Talk' ),
+'new_item'           => __( 'New Dharma Talk' ),
+'all_items'          => __( 'All Dharma Talks' ),
+'view_item'          => __( 'View Dharma Talk' ),
+'search_items'       => __( 'Search Dharma Talks' ),
+'featured_image'     => 'Poster',
+'set_featured_image' => 'Add Poster'
+);
+// The arguments for our post type, to be entered as parameter 2 of register_post_type()
+$args = array(
+'labels'            => $labels,
+'description'       => 'Holds our dharma talk post specific data',
+'public'            => true,
+'menu_position'     => 5,
+'supports'          => array( 'title', 'editor', 'author', 'custom-fields', 'revisions' ),
+'has_archive'       => true,
+'show_in_admin_bar' => true,
+'show_in_nav_menus' => true,
+'query_var'         => true,
+);
+// Call the actual WordPress function
+// Parameter 1 is a name for the post type
+// Parameter 2 is the $args array
+register_post_type( 'dharma-talk', $args);
+}
+
+/**
+ * Register meta boxes.
+ */
+function dt_register_meta_boxes() {
+    add_meta_box( 'dt-1', __( 'Talk Info', 'dt' ), 'dt_display_callback', 'dharma-talk' );
+}
+add_action( 'add_meta_boxes', 'dt_register_meta_boxes' );
+
+/**
+ * Meta box display callback.
+ *
+ * @param WP_Post $post Current post object.
+ */
+function dt_display_callback( $post ) {
+    include plugin_dir_path( __FILE__ ) . './form.php';
+}
+
+/**
+ * Save meta box content.
+ *
+ * @param int $post_id Post ID
+ */
+function dt_save_meta_box( $post_id ) {
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+    if ( $parent_id = wp_is_post_revision( $post_id ) ) {
+        $post_id = $parent_id;
+    }
+    $fields = [
+        'dt_description',
+        'dt_youtube_link',
+        'dt_series',
+        'dt_episode',
+    ];
+    foreach ( $fields as $field ) {
+        if ( array_key_exists( $field, $_POST ) ) {
+            update_post_meta( $post_id, $field, sanitize_text_field( $_POST[$field] ) );
+        }
+     }
+}
+add_action( 'save_post_dharma-talk', 'dt_save_meta_box' );
+
+/**
+ * Creates default description, if needed
+ *
+ * @param int $post_id Post ID
+ */
+function dt_save_description( $post_id ) {
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+    if ( $parent_id = wp_is_post_revision( $post_id ) ) {
+        $post_id = $parent_id;
+    }
+    if ( array_key_exists( "content", $_POST ) && trim($__POST['content']) !== '' ) {
+        return;
+    }
+    $the_post = get_post($post_id);
+    $author_id = $the_post->post_author;
+    $the_author = get_the_author_meta( 'display_name', $author_id );
+    $pretty_date = esc_html( get_the_date("l, F jS Y", $post_id) );
+    $default_description = "<p>A talk given at Berkeley Zen Center on " . $pretty_date . " by " . $the_author . ".</p>";
+    $data = array(
+        'ID' => $post_id,
+        'post_content' => $default_description
+        );
+    // unhook this function so it doesn't loop infinitely
+    remove_action( 'save_post_dharma-talk', 'dt_save_description' );
+    wp_update_post( $data );
+    add_action( 'save_post_dharma-talk', 'dt_save_description' );
+};
+add_action( 'save_post_dharma-talk', 'dt_save_description' );
+
+function dt_add_rss($qv) {
+    if (isset($qv['feed']) && !isset($qv['post_type'])) {
+        $qv['post_type'] = array('post', 'dharma-talk');
+    }
+    return $qv;
+}
+add_filter('request', 'dt_add_rss');
+
+function dt_add_to_blog($query) {
+ if ( ($query->is_author() || $query->is_home() )&& $query->is_main_query() ) {
+  $query->set( 'post_type', array( 'post', 'dharma-talk' ) );
+ }
+}
+add_action( 'pre_get_posts', 'dt_add_to_blog' );
