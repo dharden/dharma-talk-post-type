08e790928ffb8701464012414bdfa3dc4f240ea8
Dan Harden <daniel.harden@gmail.com>
2021-05-07 20:32:37 -0700

    linter stuff. will this push windows line breaks?

--- content-dharma-talk.php
+++ content-dharma-talk.php
@@ -6,25 +6,31 @@
  * @subpackage Twenty_Fourteen
  * @since Twenty Fourteen 1.0
  */
+
 ?>
 
-<?php // set up some custom fields and other useful strings
-$author = get_the_author();
-$date = esc_attr( get_the_date( 'c' ) );
-$pretty_date = esc_html( get_the_date() );
-$youtube_link = esc_attr( get_post_meta( get_the_ID(), 'dt_youtube_link', true ) );
+<?php
+// set up some custom fields and other useful strings.
+$author          = get_the_author();
+$date            = esc_attr( get_the_date( 'c' ) );
+$pretty_date     = esc_html( get_the_date() );
+$youtube_link    = esc_attr( get_post_meta( get_the_ID(), 'dt_youtube_link', true ) );
 $episode_content = get_the_powerpress_content();
-$format_class = [];
-if ( $youtube_link ) { array_push($format_class, 'format-video'); };
-if ( $episode_content ) { array_push($format_class, 'format-audio'); };
-$bio = nl2br( get_the_author_meta( 'description' ) );
+$format_class    = array();
+if ( $youtube_link ) {
+	array_push( $format_class, 'format-video' );
+};
+if ( $episode_content ) {
+	array_push( $format_class, 'format-audio' );
+};
+$bio         = nl2br( get_the_author_meta( 'description' ) );
 $the_content = get_the_content(
-				sprintf(
+	sprintf(
 					/* translators: %s: Post title. */
-					__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ),
-					the_title( '<span class="screen-reader-text">', '</span>', false )
-				)
-			);
+		__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ),
+		the_title( '<span class="screen-reader-text">', '</span>', false )
+	)
+);
 
 			wp_link_pages(
 				array(
@@ -34,18 +40,18 @@ $the_content = get_the_content(
 					'link_after'  => '</span>',
 				)
 			);
-$default_description = "<p>A talk given at Berkeley Zen Center on " . $pretty_date . " by " . $author . ".</p>";
-$talk_description = $the_content ? $the_content : $default_description;
-?>
+			$default_description = '<p>A talk given at Berkeley Zen Center on ' . $pretty_date . ' by ' . $author . '.</p>';
+			$talk_description    = $the_content ? $the_content : $default_description;
+			?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class($format_class); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( $format_class ); ?>>
 	<header class="entry-header">
 		<div class="entry-meta">
 			<a class="cat-links" href="<?php echo esc_url( get_post_type_archive_link( 'dharma-talk' ) ); ?>">Dharma Talks</a>
 		</div><!-- .entry-meta -->
 			<?php
-				if ( is_single() ) :
-					the_title( '<h1 class="entry-title">', '</h1>' );
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
 					else :
 						the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
 					endif;
@@ -53,12 +59,12 @@ $talk_description = $the_content ? $the_content : $default_description;
 
 				<div class="entry-meta">
 					<?php
-				if ( is_sticky() && is_home() && ! is_paged() ) {
-					echo '<span class="featured-post">' . __( 'Sticky', 'twentyfourteen' ) . '</span>';
-				}
+					if ( is_sticky() && is_home() && ! is_paged() ) {
+						echo '<span class="featured-post">' . __( 'Sticky', 'twentyfourteen' ) . '</span>';
+					}
 
-				// Set up and print post meta information.
-				?>
+					// Set up and print post meta information.
+					?>
 				<?php
 				printf(
 					'<span class="byline"><span class="author vcard"><a class="url fn n" href="%1$s" rel="author">%2$s</a></span></span><span class="entry-date"><a href="%3$s" rel="bookmark"><time class="entry-date" datetime="%4$s">%5$s</time></a></span>',
@@ -68,13 +74,13 @@ $talk_description = $the_content ? $the_content : $default_description;
 					$date,
 					$pretty_date
 				);
-			?>
-			<?php if ( $youtube_link ) {?>
+				?>
+			<?php if ( $youtube_link ) { ?>
 			<span class="post-format video">
 				<i class="fa"></i><?php echo get_post_format_string( 'video' ); ?>
 			</span>
 			<?php }; ?>
-			<?php if ( $episode_content ) {?>
+			<?php if ( $episode_content ) { ?>
 			<span class="post-format audio">
 				<i class="fa"></i><?php echo get_post_format_string( 'audio' ); ?>
 			</span>
@@ -83,28 +89,31 @@ $talk_description = $the_content ? $the_content : $default_description;
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 	<div class="entry-content">
-		<?php if ( $youtube_link && !is_archive() ) { ?>
-		<?php echo do_shortcode("[youtube " . $youtube_link . "]" ); ?>
+		<?php if ( $youtube_link && ! is_archive() ) { ?>
+			<?php echo do_shortcode( '[youtube ' . $youtube_link . ']' ); ?>
 		<?php }; ?>
-		<?php if ( $talk_description ) echo $talk_description; ?>
-		<?php if( $episode_content && !is_archive()) { ?>
+		<?php
+		if ( $talk_description ) {
+			echo $talk_description;}
+		?>
+		<?php if ( $episode_content && ! is_archive() ) { ?>
 		<div class="podcast">
 			<h2>Audio</h2>
 			<p>Listen to an audio-only version of this talk here: </p>
-			<?php echo $episode_content ?>
+			<?php echo $episode_content; ?>
 			<p>Subscribe to The Berkeley Zen Center Podcast: <a href="https://berkeleyzencenter.org/feed/podcast/" title="Subscribe via RSS" rel="nofollow">RSS</a></p>
 		</div>
 		<?php } ?>
 	</div><!-- .entry-content -->
-	<?php if( $bio && !is_archive() ) { ?>
+	<?php if ( $bio && ! is_archive() ) { ?>
 	<div class="about">
-		<?php if ( has_wp_user_avatar(get_the_author_meta('ID') ) ){ ?>
-		<img class="avatar circle" src="<?php echo get_wp_user_avatar_src(get_the_author_meta('ID'), 128); ?>" width="128" height="128" alt="<?php echo $author; ?>" />
+		<?php if ( has_wp_user_avatar( get_the_author_meta( 'ID' ) ) ) { ?>
+		<img class="avatar circle" src="<?php echo get_wp_user_avatar_src( get_the_author_meta( 'ID' ), 128 ); ?>" width="128" height="128" alt="<?php echo $author; ?>" />
 		<?php } ?>
 		<div class="bio">
 			<h3>About <?php echo $author; ?></h3>
 			<p><?php echo $bio; ?></p>
-			<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) ?>">More from <?php echo $author; ?></a>
+			<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">More from <?php echo $author; ?></a>
 		</div>
 	</div><!-- .entry-meta -->
 	<?php } ?>
--- form.php
+++ form.php
@@ -1,20 +1,20 @@
 <div class="dt_box">
-    <style scoped>
-        .dt_box{
-            display: grid;
-            grid-template-columns: max-content 1fr;
-            grid-row-gap: 10px;
-            grid-column-gap: 20px;
-        }
-        .dt_field{
-            display: contents;
-        }
-    </style>
-    <p class="meta-options dt_field">
-        <label for="dt_youtube_link">YouTube Link</label>
-        <input id="dt_youtube_link"
-            type="text"
-            name="dt_youtube_link"
-            value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'dt_youtube_link', true ) ); ?>">
-    </p>
+	<style scoped>
+		.dt_box{
+			display: grid;
+			grid-template-columns: max-content 1fr;
+			grid-row-gap: 10px;
+			grid-column-gap: 20px;
+		}
+		.dt_field{
+			display: contents;
+		}
+	</style>
+	<p class="meta-options dt_field">
+		<label for="dt_youtube_link">YouTube Link</label>
+		<input id="dt_youtube_link"
+			type="text"
+			name="dt_youtube_link"
+			value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'dt_youtube_link', true ) ); ?>">
+	</p>
 </div>
