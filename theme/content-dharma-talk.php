a3066b7e5cb25083dc44caccacc9da6a7d59f175
Dan Harden <daniel.harden@gmail.com>
2021-05-11 17:18:08 -0700

    template var refactor

--- theme/content-dharma-talk.php
+++ theme/content-dharma-talk.php
@@ -12,15 +12,16 @@
 <?php
 // set up some custom fields and other useful strings.
 $author          = get_the_author();
+$author_id       = get_the_author_meta( 'ID' );
 $date            = esc_attr( get_the_date( 'c' ) );
 $pretty_date     = esc_html( get_the_date() );
 $youtube_link    = esc_attr( get_post_meta( get_the_ID(), 'dt_youtube_link', true ) );
-$episode_content = get_the_powerpress_content();
+$podcast_player = get_the_powerpress_content();
 $format_class    = array();
 if ( $youtube_link ) {
 	array_push( $format_class, 'format-video' );
 };
-if ( $episode_content ) {
+if ( $podcast_player ) {
 	array_push( $format_class, 'format-audio' );
 };
 $bio         = nl2br( get_the_author_meta( 'description' ) );
@@ -68,7 +69,7 @@ $the_content = get_the_content(
 				<?php
 				printf(
 					'<span class="byline"><span class="author vcard"><a class="url fn n" href="%1$s" rel="author">%2$s</a></span></span><span class="entry-date"><a href="%3$s" rel="bookmark"><time class="entry-date" datetime="%4$s">%5$s</time></a></span>',
-					esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+					esc_url( get_author_posts_url( $author_id ) ),
 					esc_html( $author ),
 					esc_url( get_permalink() ),
 					esc_html( $date ),
@@ -80,7 +81,7 @@ $the_content = get_the_content(
 				<i class="fa"></i><?php echo esc_html( get_post_format_string( 'video' ) ); ?>
 			</span>
 			<?php }; ?>
-			<?php if ( $episode_content ) { ?>
+			<?php if ( $podcast_player ) { ?>
 			<span class="post-format audio">
 				<i class="fa"></i><?php echo esc_html( get_post_format_string( 'audio' ) ); ?>
 			</span>
@@ -109,14 +110,14 @@ $the_content = get_the_content(
 			);
 		}
 		?>
-		<?php if ( $episode_content && ! is_archive() ) { ?>
+		<?php if ( $podcast_player && ! is_archive() ) { ?>
 		<div class="podcast">
 			<h2>Audio</h2>
 			<p>Listen to an audio-only version of this talk here: </p>
-			<?php $episode_content; ?>
+			<?php $podcast_player; ?>
 			<?php
-			if ( $episode_content ) {
-				echo $episode_content;
+			if ( $podcast_player) {
+				echo $podcast_player;
 			}
 			?>
 			<p>Subscribe to The Berkeley Zen Center Podcast: <a href="https://berkeleyzencenter.org/feed/podcast/" title="Subscribe via RSS" rel="nofollow">RSS</a></p>
@@ -125,13 +126,13 @@ $the_content = get_the_content(
 	</div><!-- .entry-content -->
 	<?php if ( $bio && ! is_archive() ) { ?>
 	<div class="about">
-		<?php if ( has_wp_user_avatar( get_the_author_meta( 'ID' ) ) ) { ?>
-		<img class="avatar circle" src="<?php echo esc_attr( get_wp_user_avatar_src( get_the_author_meta( 'ID' ), 128 ) ); ?>" width="128" height="128" alt="<?php echo esc_attr( $author ); ?>" />
+		<?php if ( has_wp_user_avatar( $author_id ) ) { ?>
+		<img class="avatar circle" src="<?php echo esc_attr( get_wp_user_avatar_src( $author_id, 128 ) ); ?>" width="128" height="128" alt="<?php echo esc_attr( $author ); ?>" />
 		<?php } ?>
 		<div class="bio">
 			<h3>About <?php echo esc_html( $author ); ?></h3>
 			<p><?php echo esc_html( $bio ); ?></p>
-			<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">More from <?php echo esc_html( $author ); ?></a>
+			<a href="<?php echo esc_url( get_author_posts_url( $author_id ) ); ?>">More from <?php echo esc_html( $author ); ?></a>
 		</div>
 	</div><!-- .entry-meta -->
 	<?php } ?>
